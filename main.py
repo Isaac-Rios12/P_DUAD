f8ff4782816f849b196f5c060d872474684201e7
Isaac-Rios12 <rios_joan12@yahoo.com>
2024-03-11 20:00:55 -0600

    se agrega modifica la logica de seguir agregando, para esto se agrega un moulo para esto, tambien se crea un modulo para ver el top 3 promedios

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ def ask_grade(subject):
         except ValueError:
             print("gas ingresado un valor no valido...")
             
+def ask_if_keep_adding_student():
+    try:
+        keep_asking = int(input("Elige una opcion: 1.Ingresar otra estudiante 2.Salir..."))
+        if keep_asking == 1:
+            return True
+        elif keep_asking == 2:
+            print("Redirigiendo al mnu principal...")
+            return False
+        else:
+            print("opcion fuera de rango")
+    except ValueError:
+        print("valor no valido")
+        
+
 def add_student():
     while True:
         try:
@@ -23,6 +37,7 @@ def add_student():
             english_grade = ask_grade("Ingles")
             social_grade = ask_grade("Sociales")
             science_grade = ask_grade("Ciencias")
+            average_student = (spanish_grade + english_grade + science_grade + social_grade) / 4
             
             student = {
                 "Name": name_student,
@@ -30,21 +45,21 @@ def add_student():
                 "Spanish grade": spanish_grade,
                 "English grade": english_grade,
                 "Social grade": social_grade,
-                "Science grade": science_grade
+                "Science grade": science_grade,
+                "Average": average_student
             }
             students_list.append(student)
             
+            #ask_if_keep_adding_student()
             
-            keep_asking = int(input("Elige una opcion: 1.Ingresar otra estudiante 2.Salir..."))
-            if keep_asking == 2:
-                print("Redirigiendo al mnu principal...")
+            if not ask_if_keep_adding_student():
                 break
             
         except ValueError:
             print("error encontrado")
             
-    print(students_list)
-    return students_list
+    #print(students_list)
+    #return students_list
 
 def show_students():
     for student in students_list:
@@ -54,8 +69,22 @@ def show_students():
         print("Nota de ingles:   ", student["English grade"])
         print("Nota de sociales: ", student["Social grade"])
         print("Nota de ciencias: ", student["Science grade"])
+        print("Promedio:         ", student["Average"])
         print("-------------------------------------------------")
 
+def show_top_3_best_average_grade():
+    #sort_students_list = students_list.sort(key=lambda x: x["Average"])
+    #students_list.sort(key=lambda x: x["Average"], reverse=True)
+    #print(students_list)
+    #print()
+    sort_students_list = sorted(students_list, key=lambda x: x["Average"], reverse=True)
+    print("Los mejores 3 estudiantes son:")
+    for i in range(min(3, len(sort_students_list))):
+        student = sort_students_list[i]
+        print(f"Nombre: {student['Name']}")
+        print(f"Sección: {student['Section']}")
+        print(f"Promedio: {student['Average']}")
+        print("--------------------")
 def menu():
     while True:
     
@@ -75,7 +104,7 @@ def menu():
                 elif option == 2:
                     show_students()
                 elif option == 3:
-                    print("has entrado a la opcion 3")
+                    show_top_3_best_average_grade()
                 elif option == 4:
                     print("has entrado a la opcion 4")
                 elif option == 5:
