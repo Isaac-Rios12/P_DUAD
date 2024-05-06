e8f1d2f5b38b1c92ee7d2c69dbbea81b44e5a8cc
Isaac-Rios12 <rios_joan12@yahoo.com>
2024-05-06 15:44:56 -0600

    Completed the exercise #2

--- Semana12/Ejercicio2.py
+++ Semana12/Ejercicio2.py
@@ -43,8 +43,25 @@ class Rectangle(Shape):
     def calculate_area(self):
         return self.length * self.width
     
-circle = Circle(10)
+radio = float(input("Ingresa el radio del círculo: "))
+circle = Circle(radio)
 calculate_circle_area = circle.calculate_area()
 calculate_circle_perimeter = circle.calculate_perimeter()
-print(f'el area es de...{calculate_circle_area}')
-print(f'el perimetro es de...{calculate_circle_perimeter}')
+print(f'El área del círculo es: {calculate_circle_area}')
+print(f'El perímetro del círculo es: {calculate_circle_perimeter}')
+
+
+side_length = float(input("Ingresa el lado del cuadrado: "))
+square = Square(side_length)
+calculate_square_area = square.calculate_area()
+calculate_square_perimeter = square.calculate_perimeter()
+print(f'El área del cuadrado es: {calculate_square_area}')
+print(f'El perímetro del cuadrado es: {calculate_square_perimeter}')
+
+length = float(input("Ingresa la longitud del rectángulo: "))
+width = float(input("Ingresa el ancho del rectángulo: "))
+rectangle = Rectangle(length, width)
+calculate_rectangle_area = rectangle.calculate_area()
+calculate_rectangle_perimeter = rectangle.calculate_perimeter()
+print(f'El área del rectángulo es: {calculate_rectangle_area}')
+print(f'El perímetro del rectángulo es: {calculate_rectangle_perimeter}')
