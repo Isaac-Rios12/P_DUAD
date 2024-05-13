b0545c4fcc66092fac211de98cb92dfa2a6291cc
Isaac-Rios12 <rios_joan12@yahoo.com>
2024-05-13 08:13:21 -0600

    exercise #1, money withdrawal correction...

--- Semana12/Ejercicio1.py
+++ Semana12/Ejercicio1.py
@@ -6,8 +6,13 @@ class BankAccount:
     def _add_Balance(self, amount):
         self._balance += amount
 
-    def _substract_balance(self, amount):
-        self._balance -= amount
+    def _subtract_balance(self, amount):
+        if self._balance < amount:
+            #print("Fondos insuficientes...")
+            return False
+        else:
+            self._balance -= amount
+            return True
 
 
 class SavingsAccount(BankAccount):
@@ -19,17 +24,19 @@ class SavingsAccount(BankAccount):
         self._add_Balance(amount)
         print(f"el monto agregado es de...{amount}")
 
-    def check_withdrawal(self, substract):
-        if substract > self._balance:
-            print("Fondos insuficientes...")
-        elif self._balance - substract < self.min_balance:
-            print(f"Monto supera el min_balance... total en cuenta: {self._balance}... disponible a retirar: {(self._balance-self.min_balance)}")
+    def check_withdrawal(self, amount):
+        if self._balance >= amount:
+            if self._balance - amount >= self.min_balance:
+                self._subtract_balance(amount)
+                print("Transaccion realizada...")
+                print(f"Su balance es de {self._balance}")
+            else:
+                print(f"Monto supera el min_balance... total en cuenta: {self._balance}... disponible a retirar: {(self._balance - self.min_balance)}")
         else:
-            self._substract_balance(substract)
-            print("Transaccion realizada...")
-            print(f"Su balance es de {self._balance}")
+            print("Fondos insuficientes...")
 
-savings_account = SavingsAccount()      
+
+savings_account = SavingsAccount()     
 amount = int(input("Ingrese el monto que desea depositar..."))
 savings_account.add_balance(amount)
 substract = int(input("Ingrese el monto a retirar..."))
