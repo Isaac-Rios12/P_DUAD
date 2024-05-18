69b006df88114720ff47a7a6cae787b726e0bdd9
Isaac-Rios12 <rios_joan12@yahoo.com>
2024-05-18 08:13:23 -0600

    Exercise #1, Corrected observations 1-Min_balance configurable, 2-removed repeated line of condition

--- Semana12/Ejercicio1.py
+++ Semana12/Ejercicio1.py
@@ -11,33 +11,29 @@ class BankAccount:
             print("Fondos insuficientes...")
             return False
         else:
-            print("Monto agregado...")
             self._balance -= amount
+            print("Monto retirado...")
+            print(f"Su balance es de {self._balance}")
             return True
 
 
 class SavingsAccount(BankAccount):
-    def __init__(self):
+    def __init__(self, min_balance):
         super().__init__()
-        self.min_balance = 100
+        self.min_balance = min_balance
 
     def add_balance(self, amount):
         super().add_balance(amount)
         print(f"el monto agregado es de...{amount}")
 
     def subtract_balance(self, amount):
-        if self._balance >= amount:
-            if self._balance - amount >= self.min_balance:
-                super().subtract_balance(amount)
-                print("Transaccion realizada...")
-                print(f"Su balance es de {self._balance}")
-            else:
-                print(f"Monto supera el min_balance... total en cuenta: {self._balance}... disponible a retirar: {(self._balance - self.min_balance)}")
+        if self._balance - amount >= self.min_balance:
+                super().subtract_balance(amount)      
         else:
-            print("Fondos insuficientes...")
+                print(f"Operacion no realizada... total en cuenta: {self._balance}... disponible a retirar: {(self._balance - self.min_balance)}")
 
-
-savings_account = SavingsAccount()     
+min_balance = int(input("Ingrese el balance minimo de su cuenta de ahorros..."))
+savings_account = SavingsAccount(min_balance)     
 amount = int(input("Ingrese el monto que desea depositar..."))
 savings_account.add_balance(amount)
 substract = int(input("Ingrese el monto a retirar..."))
