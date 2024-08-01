3be56a28b82ab55c165805caa488e3b0aaf7112f
Isaac-Rios12 <rios_joan12@yahoo.com>
2024-07-31 18:51:29 -0600

    exercise #2 modified

--- Semana15/Ejercicio2.py
+++ Semana15/Ejercicio2.py
@@ -1,16 +1,16 @@
 
-def inverted_bubble_sort(list_to_oder):
-    for outer_index in range(len(list_to_oder) - 1, 0, -1): 
+def inverted_bubble_sort(list_to_order):
+    for outer_index in range(len(list_to_order) - 1, 0, -1): 
         has_made_changes = False
-        for index in range(len(list_to_oder) - 1, len(list_to_oder) - 1 - outer_index, -1):
-            current_number = list_to_oder[index]
-            next_number = list_to_oder[index - 1]
+        for index in range(len(list_to_order) - 1, len(list_to_order) - 1 - outer_index, -1):
+            current_number = list_to_order[index]
+            next_number = list_to_order[index - 1]
 
             print(f'-- Iteracion {outer_index}, {index}. Elemento actual: {current_number}, Siguiente elemento: {next_number}')
 
             if current_number < next_number:
-                list_to_oder[index] = next_number
-                list_to_oder[index - 1] = current_number
+                list_to_order[index] = next_number
+                list_to_order[index - 1] = current_number
                 has_made_changes = True
 
         if not has_made_changes:
