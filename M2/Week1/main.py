41e48eacfe1ab243284d148ff4aa843bee6fd965
Isaac-Rios12 <rios_joan12@yahoo.com>
2024-10-28 18:40:25 -0600

    Add initial M2 code to Week1BACKEND branch

---
+++ M2/Week1/main.py
@@ -0,0 +1,101 @@
+from flask import Flask, jsonify, request
+from Week1.data import Task
+from Week1.logic import check_id, remove_task, is_valid_status
+
+app = Flask(__name__)
+
+task_instance = Task()
+task_instance.import_data("tareas.json")
+
+
+@app.route('/Tasks', methods=["GET", "POST"])
+def create_task():
+
+    if request.method == "GET":
+        data = task_instance.get_task()
+        return jsonify(data)
+    else:
+        try:
+ 
+            if "id" not in request.json:
+                raise ValueError("Debes ingresar el id")
+            if "titulo" not in request.json:
+                raise ValueError("Debes ingresar el titulo")
+            if "descripcion" not in request.json:
+                raise ValueError("Debes ingresar la descripcion")
+            if "estado" not in request.json:
+                raise ValueError("Debes ingresar el etsado")
+
+            state = request.json.get("estado")
+            state_error = is_valid_status(state)
+            if state_error:
+                return jsonify(state_error), 400
+            
+
+            id = request.json['id']
+            exists, tarea= check_id(id, task_instance)
+
+            if not exists:
+
+                new_data = {
+                    "id": request.json.get("id"),
+                    "titulo": request.json.get("titulo"),
+                    "descripcion": request.json.get("descripcion"),
+                    "estado": state
+                }
+
+                task_instance.add_task(new_data)
+                task_instance.export_data("tareas.json")
+                return jsonify(message="Tarea agregada con éxito"), 201
+                
+
+            else:
+                return jsonify(message="Id ya registrado...."), 400
+
+        except ValueError as ex:
+            return jsonify(message=str(ex)), 400
+        except Exception as ex:  
+            return jsonify(message=str(ex)), 500
+
+@app.route('/Tasks/<string:id>', methods=["DELETE"])
+def delete_task(id):
+
+    if not id or id.strip()=="":
+        return jsonify(message="ID no proporcionado o invalido"), 400
+    
+    exists, tarea = check_id(id, task_instance)
+
+    if exists:
+        remove_task(tarea, task_instance)
+        task_instance.export_data("tareas.json")  
+        return jsonify(message="Tarea eliminada con éxito"), 200
+    else:
+        return jsonify(message="Tarea no encontrada"), 404  
+
+
+@app.route('/Tasks/<string:id>', methods=["PATCH"])
+def update_task(id):
+
+    exists, tarea = check_id(id, task_instance)
+
+    new_status = request.json.get("estado")
+
+    if new_status is None or new_status.strip() == "":
+        return jsonify(message="El campo estado es requerido"), 400
+
+    if not exists:
+        return jsonify(message="Tarea no encontrada"), 400
+
+    state_error = is_valid_status(new_status)
+    if state_error:
+        return jsonify(state_error), 400
+
+    if tarea["estado"] == new_status:
+        return jsonify(message="No se realizan cambios, estado es el mismo"), 200
+    
+    response = task_instance.update_task_logic(tarea, new_status)
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", debug=True)
---
+++ M2/extra_exercise/main.py
@@ -0,0 +1,110 @@
+from flask.views import MethodView
+from flask import Flask, jsonify, request
+from extra_exercise.logic import check_id, is_valid_status, remove_task
+from extra_exercise.data import Task
+
+
+app = Flask(__name__)
+
+
+class TaskAPI(MethodView):
+
+    def __init__(self):
+        self.task_instance = Task()
+        self.task_instance.import_data("tareas.json")
+
+    def get(self):
+        data = self.task_instance.get_task()
+        return jsonify(data)
+
+
+    def post(self):
+        try:
+            if "id" not in request.json:
+                raise ValueError("Debes ingresar el id")
+            if "titulo" not in request.json:
+                raise ValueError("Debes ingresar el titulo")
+            if "descripcion" not in request.json:
+                raise ValueError("Debes ingresar la descripcion")
+            if "estado" not in request.json:
+                raise ValueError("Debes ingresar el estado")
+
+            state = request.json.get("estado")
+            state_error = is_valid_status(state)
+
+            if state_error:
+                return jsonify(state_error), 400
+
+            id = request.json['id']
+            exists, tarea = check_id(id, self.task_instance)
+
+            if not exists:
+                new_data = {
+                    "id": request.json.get("id"),
+                    "titulo": request.json.get("titulo"),
+                    "descripcion": request.json.get("descripcion"),
+                    "estado": request.json.get("estado")
+                }
+
+                self.task_instance.add_task(new_data)
+                self.task_instance.export_data("tareas.json")
+                return jsonify(message="Tarea agregada con eexito"), 200
+            
+            else:
+                return jsonify(message="Id de tarea ya existe"), 400
+            
+        except ValueError as ex:
+            return jsonify(message=str(ex)), 400
+        except ValueError as ex:
+            return jsonify(message=str(ex)), 500
+
+
+    def delete(self, id):
+        if not id or id.strip()=="":
+            return jsonify(message="Id no proporcionado o invalido"), 400
+        
+        exists, tarea = check_id(id, self.task_instance)
+
+        if exists:
+            remove_task(tarea, self.task_instance)
+            self.task_instance.export_data("tareas.json")
+            return jsonify(message="tarea eliminadaa....."), 200
+        else:
+            return jsonify(message="Tarea no enconntrada"), 404
+
+    def patch(self, id):
+
+        exists, tarea = check_id(id, self.task_instance)
+
+        new_status = request.json.get("estado")
+
+        if new_status is None or new_status.strip() == "":
+            return jsonify(message="El campo estado es requerido"), 400
+
+        if not exists:
+            return jsonify(message="La tarea no existe"), 400
+
+        state_error = is_valid_status(new_status)
+
+        if state_error:
+            return jsonify(state_error), 400
+        
+        if tarea['estado'] == new_status:
+            return jsonify(message="No se realizan cambios, el estado es el mismo"), 200
+
+        response = self.task_instance.update_task_logic(tarea, new_status)
+        return response
+
+def register_api(app, name):
+    task_view = TaskAPI.as_view(f"{name}-item")
+    app.add_url_rule(f"/{name}", view_func=task_view, methods=["GET", "POST"])
+    app.add_url_rule(f"/{name}/<string:id>", view_func=task_view, methods=["DELETE", "PATCH"])
+
+register_api(app, "Tasks")
+
+if __name__ == "__main__":
+    app.run(host="localhost", debug=True)
+
+
+
+
